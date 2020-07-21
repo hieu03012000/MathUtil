25facd04124a8cc0b8a040f839004691eb084d70
hieu03012000 <hieu03012000@gmail.com>
2020-07-21 08:24:14 +0700

    add the exeption handler for cF()

--- src/hieunt/util/MathUtils.java
+++ src/hieunt/util/MathUtils.java
@@ -9,11 +9,15 @@ package hieunt.util;
  *
  * @author WELCOME
  */
-
 //class nay la tien ich ho tro cac phep tinh toan
 //day la tien ich y dung chung cho moij noi do do se la ham static
 public class MathUtils {
+
     public static long computerFactorial(int n) {
+        //khong chap nhan giai thua am va n>15 vi qua lon
+        if (n < 0 || n > 15) {
+            throw new IllegalArgumentException("n must be >=0 and <= 15");
+        }
         long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
