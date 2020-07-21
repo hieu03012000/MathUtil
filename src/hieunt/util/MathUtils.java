89fa044883970c7fb448bb5157a512964a0ec253
hieu03012000 <hieu03012000@gmail.com>
2020-07-21 09:49:53 +0700

    add cF() with recurtion

--- src/hieunt/util/MathUtils.java
+++ src/hieunt/util/MathUtils.java
@@ -12,16 +12,20 @@ package hieunt.util;
 //class nay la tien ich ho tro cac phep tinh toan
 //day la tien ich y dung chung cho moij noi do do se la ham static
 public class MathUtils {
-
+    
     public static long computerFactorial(int n) {
         //khong chap nhan giai thua am va n>15 vi qua lon
         if (n < 0 || n > 15) {
             throw new IllegalArgumentException("n must be >=0 and <= 15");
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        return result;
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+        return n * computerFactorial(n - 1);
     }
 }
